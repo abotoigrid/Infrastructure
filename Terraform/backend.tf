fbd3ebc82cc9f2c6b50eb0f1adb0f22111626963
Botoi Adrian <abotoi@griddynamics.com>
2024-12-31 09:14:23 +0200

    Add Terraform folder, with S3 bucket and backend .tfstate

---
+++ Terraform/backend.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "s3" {
+    bucket = "abt-my-bucket-capstone-project"
+    key    = "terraform/terraform.tfstate"
+    region = "eu-central-1"
+  }
+}
---
+++ Terraform/terraform_bucket.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = "abt-my-bucket-capstone-project"
+  tags = {
+    Name    = "Capstone Project Bucket"
+    Owner   = "abotoi"
+    Project = "2024_internship_moldova"
+  }
+}
