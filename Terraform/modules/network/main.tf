1a9daf05a0384140e7070caf1048c8b64b338f01
Botoi Adrian <abotoi@griddynamics.com>
2025-01-02 10:24:03 +0200

    Modify subnet's availability_zone

--- Terraform/modules/network/main.tf
+++ Terraform/modules/network/main.tf
@@ -6,7 +6,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "main" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.subnet_cidr_block
-  availability_zone = "us-east-1a"
+  availability_zone = "eu-central-1a"
   tags              = var.tags
 }
 
